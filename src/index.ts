dfd8780692d4dc187f09160c3cbfaf7e9d30b342
gaetanpierru <pierrugaetan0@gmail.com>
2023-01-10 17:04:15 +0100

    test 11

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ require('dotenv');
 const app = express()
 
 app.get('/hello', (req, res) => res.send('Hello Simplon by gaetan'));
-app.get('/testDocker', (req, res) => res.send('Hello test Docker' + process.env.id));
+app.get('/testDocker', (req, res) => res.send('Hello test Docker ' + process.env.id));
 
 app.listen(3004, ()=> console.log("Application démarrée sur le port 3004"));
